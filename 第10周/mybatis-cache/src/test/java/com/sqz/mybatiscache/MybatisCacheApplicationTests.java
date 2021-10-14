0e3a651892e158bbad1f45e5815c51b6260bde95
sunqingzhong <sunqingzhong@unisound.com>
2021-10-14 08:17:08 +0800

    ==

---
+++ 第10周/mybatis-cache/src/test/java/com/sqz/mybatiscache/MybatisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqz.mybatiscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
